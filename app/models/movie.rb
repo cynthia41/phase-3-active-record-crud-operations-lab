5a1af6719f383e8731cf806f16d81642993ec356
cynthia41 <cynthiaadhiambo41@gmail.com>
2023-06-03 20:36:49 +0300

    completed assignment

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,37 @@
 class Movie < ActiveRecord::Base
-
+ def self.create_with_title(title)
+    movie = Movie.new(title: "This is a title.")
+    movie.save
+    movie
+ end
+ def self.first_movie
+    Movie.first
+ end
+ def self.last_movie
+    Movie.last
+ end
+ def self.movie_count
+    Movie.count
+ end
+ def self.find_movie_with_id(id)
+    Movie.find_by_id(id)
+ end
+ def self.find_movie_with_attributes(attr)
+    Movie.find_by(attr)
+ end
+ def self.find_movies_after_2002
+    Movie.where('release_date > 2002')
+ end
+ def self.update_all_titles(title)
+    Movie.update(title: title)
+ end
+ def update_with_attributes(attributes)
+    update(attributes)
+ end
+ def self.delete_by_id(id)
+    Movie.destroy(id)
+  end
+ def self.delete_all_movies
+    Movie.destroy_all
+ end
 end
